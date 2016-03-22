0f9c365bed0b629d9b12844283605de314a04a58
Nick Marucci <npm1514@gmail.com>
2016-03-22 14:21:31 -0600

    first add of main docs

---
+++ public/app.js
@@ -0,0 +1,15 @@
+angular.module("DuoLingoJS", ['ui.router'])
+  .config(function($urlRouterProvider, $stateProvider) {
+      $stateProvider
+      .state("home", {
+        url: "/home",
+        templateUrl: "home.html"
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'templates/about.html'
+      });
+      $urlRouterProvider
+        .otherwise('/sell');
+  });
+  
